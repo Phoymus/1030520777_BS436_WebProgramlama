33ffa3edc0104c8bbc50b63cade52c98ff1877c6
Osman İNCİR <osmanincir.bm@gmail.com>
2021-03-10 17:37:36 +0300

    Odev01_Son

--- odev01/code.js
+++ odev01/code.js
@@ -18,7 +18,7 @@
   *  Bu liste tüm kartların resim uzantılarını
   *  ve resmin sınıfını içerisinde barındırır.
   */
-const img = [
+const imgs = [
     {
         src: "img1.jpg",
         class: "cat"
@@ -41,21 +41,21 @@ let found = false;  // Kedinin bulunma durumunu temsil eder.
  * @param imgId
  */
 const showCard = (imgId) =>{
-    const index = Math.floor(Math.random()*img.length) // images listesinde rastgele resim indexi seçer.
+    const index = Math.floor(Math.random()*imgs.length) // images listesinde rastgele resim indexi seçer.
 
-    document.getElementById(imgId).src = img[index].src; // Seçilen resim uzantısını gönderilen resim id sinin uzantısı ile değiştirir.
+    document.getElementById(imgId).src = imgs[index].src; // Seçilen resim uzantısını gönderilen resim id sinin uzantısı ile değiştirir.
     document.getElementById(imgId).onclick = null;  // Resimin tıklanılabilirligini kaldırır.
 
-    if (img[index].class === "cat" && img.length  > 1){
+    if (imgs[index].class === "cat" && imgs.length  > 1){
         // Rastgele seçilen resmin kedi olması ve halen images listesinde 1 den fazla eleman olup olmadığı kontrolü.
 
         document.getElementById("areaId").innerHTML = "Kazandın!!! Tebrik Ederiz :) Yeni bir oyun oynamak istersen "
             +"buraya".link("index.html")+"tıklayabilirsin.";    // İçerisinde anasayfa linki olan kazanma yazısını çıktı verir.
         found = true;
     }
-    img.splice(index,1); // Seçilen resmin tekrar seçilmemesi için resmi images listesinden çıkarır.
+    imgs.splice(index,1); // Seçilen resmin tekrar seçilmemesi için resmi images listesinden çıkarır.
 
-    if(img.length === 1  && !found){
+    if(imgs.length === 1  && !found){
         // images listesinin içerisinde sadece 1 resim kalmış ve kedi bulunmamış olup olmadıgı kontrolü.
 
         document.getElementById("areaId").innerHTML = "Kaybettin:( Yeni bir oyun oynamak istersen "
